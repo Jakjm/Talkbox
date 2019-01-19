b285839aa649c061d2e3f3d3e2b1c15c82fbad71
rohantalkad747 <30855683+rohantalkad747@users.noreply.github.com>
2019-01-18 21:12:05 -0500

    added formatchecker method

--- src/filehandler/FileIO.java
+++ src/filehandler/FileIO.java
@@ -1,46 +1,43 @@
 package filehandler;
 
 import static java.nio.file.StandardCopyOption.*;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIO {
 	/**
-	 * Puts file into folder specified by path.
-	 * @param file The file of .wav and .aiff format
-	 * @param dir The new path in backslashes from the root
+	 * Utilities for audio file handling.
+	 * @author Alberto Mastrofrancesco
+	 * @author Jordan Malek
+	 * @author Rohan Talkad
+	 * @version 1.0
 	 */
-	public static void addFile(File toWrite, String dir) {
-		try {
-			File newFile = new File(toWrite, dir);
-			FileOutputStream fileStream = new FileOutputStream(newFile) ;
-			DataOutputStream dataStream = new DataOutputStream(fileStream);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+	
+	/**
+	 * Verifies if file is in .wav format
+	 * @param file - The file instance to be checked
+	 */
+	public static boolean checkFileFormat(File file)  {
+		String path = file.getPath();
+		String ext = path.substring(path.lastIndexOf(".") + 1);
+		boolean isWav = ext.equals(".wav");
+		return isWav;
 	}
 	/**
 	 * Puts file into folder specified by path.
-	 * @param filePath The absolute path of file in string format
+	 * @param File The target file to be moved
 	 * @param dir The absolute path of destination in string format
 	 */
-	public static void addFile(String filePath, String dir) {
+	public static void addFile(File file, String dir) {
 		try {
-			Path orig = Paths.get(filePath);
+			Path orig = Paths.get(file.getPath());
 			Path dest = Paths.get(dir);
 			Files.move(orig, dest, ATOMIC_MOVE);
 		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		catch (InvalidPathException e) {
+		catch ( IOException e) {
 			e.printStackTrace();
 		}
 	}
