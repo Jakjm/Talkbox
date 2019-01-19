3dce04d088bf1c3733280b436900e4c3f4ae4f6c
rohantalkad747 <30855683+rohantalkad747@users.noreply.github.com>
2019-01-19 17:41:44 -0500

    changed file verification methods

--- src/filehandler/FileIO.java
+++ src/filehandler/FileIO.java
@@ -7,6 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class FileIO {
 	/**
 	 * Utilities for audio file handling.
@@ -17,17 +23,41 @@ public class FileIO {
 	 */
 	
 	/**
-	 * Verifies if file is in .wav format
-	 * @param file - The file instance to be checked
+	 * Verifies if file format is WAVE by checking its path.
+	 * @param path - The absolute path in string form
+	 * @return whether the file format is WAVE
 	 */
-	public static boolean checkFileFormat(File file)  {
-		String path = file.getPath();
-		String ext = path.substring(path.lastIndexOf(".") + 1); 
-		return ext.equals(".wav");
+	public static boolean checkFileFormatByPath(String path)  {
+		boolean isWave = false;
+		// if the extension is .wav we continue to check its format
+		String ext = path.substring(path.lastIndexOf(".") + 1);
+		if (ext.equals("wav")) {
+			File toCheck = new File(path);
+			isWave = FileIO.checkFileFormat(toCheck);
+		}
+		return isWave;
+		
+	}
+	/**
+	 * Verifies if a file format is WAVE by checking its format.
+	 * @param file - the file to be verified
+	 * @return whether the file format is WAVE
+	 */
+	public static boolean checkFileFormat(File file) {
+		boolean isWave = false;
+		try {
+			// return file format type from the audio input stream of the file
+			AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+			isWave = fileFormat.getType().equals(AudioFileFormat.Type.WAVE);
+		}
+		catch (IOException | UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		}
+		return isWave;
 	}
 	/**
 	 * Puts file into folder specified by path.
-	 * @param File The target file to be moved
+	 * @param File The target file to be moved.
 	 * @param dir The absolute path of destination in string format
 	 */
 	public static void addFile(File file, String dir) {
