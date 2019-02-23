54448eb5f997e09b2b46116982f8e70f562b911a
jakjm <jakj3m@gmail.com>
2019-02-23 18:00:02 -0500

    Slight bug fix

--- src/browsing/FileSelector.java
+++ src/browsing/FileSelector.java
@@ -136,7 +136,10 @@ public class FileSelector extends JFrame implements ActionListener {
 
 		openDirectory(homeFile);
 	}
-
+	public void setVisible(boolean b) {
+		openDirectory(this.currentFile);
+		super.setVisible(b);
+	}
 	public void reset() {
 		openDirectory(homeFile);
 	}
